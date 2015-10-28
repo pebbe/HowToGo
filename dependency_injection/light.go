53503ac2b66f2d3893a3cc4142e5a0988e9b1819
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-10-28 12:41:15 +0100

    modified: dependency_injection/light.go

--- dependency_injection/light.go
+++ dependency_injection/light.go
@@ -10,57 +10,41 @@ type OnOff interface {
 	Off() error
 }
 
-// Light implements OnOff
 type Light struct {
-	age *int
+	age int
 }
 
-func NewLight() Light {
-	return Light{
-		age: new(int),
-	}
-}
-
-func (light Light) On() error {
-	*light.age++
-	if *light.age == 5 {
+func (light *Light) On() error {
+	light.age++
+	if light.age == 5 {
 		return fmt.Errorf("The light broke")
 	}
 	fmt.Println("The light is on")
 	return nil
 }
 
-func (light Light) Off() error {
+func (light *Light) Off() error {
 	fmt.Println("The light is off")
 	return nil
 }
 
-// Switch uses an OnOff
 type Switch struct {
 	isOn   bool
-	outlet *OnOff
-}
-
-func NewSwitch(outlet *OnOff) Switch {
-	return Switch{
-		isOn:   false,
-		outlet: outlet,
-	}
+	outlet OnOff
 }
 
 func (s *Switch) Flip() error {
 	s.isOn = !s.isOn
 	if s.isOn {
-		return (*s.outlet).On()
+		return s.outlet.On()
 	}
-	return (*s.outlet).Off()
+	return s.outlet.Off()
 }
 
 func main() {
 
-	var l OnOff
-	l = NewLight()
-	s := NewSwitch(&l)
+	l := Light{}
+	s := Switch{outlet: &l} // *Light implements OnOff
 
 	for {
 		err := s.Flip()
