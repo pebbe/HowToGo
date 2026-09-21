5677c8c0f7552d930cbb46c119b4b74a80c0da3e
Peter Kleiweg <kleiweg@ziggo.nl>
2026-09-21 18:38:50 +0200

    how to remove accents

---
+++ remove_accents/go.mod
@@ -0,0 +1,5 @@
+module local
+
+go 1.27.1
+
+require golang.org/x/text v0.42.0
---
+++ remove_accents/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+type RT struct {
+	rt *unicode.RangeTable
+}
+
+func NewRT(rt *unicode.RangeTable) RT {
+	return RT{rt: rt}
+}
+
+func (t RT) Contains(r rune) bool {
+	return unicode.Is(t.rt, r)
+}
+
+func main() {
+	s := "Yoùr Śtring šđčćžŠĐČĆŽ Ötzi's Nationalität èàì Ĳĳ"
+	b := make([]byte, len(s))
+
+	mn := NewRT(unicode.Mn) // Mn: nonspacing marks
+
+	t := transform.Chain(norm.NFKD, runes.Remove(mn), norm.NFKC)
+	_, _, e := t.Transform(b, []byte(s), true)
+	if e != nil {
+		panic(e)
+	}
+
+	fmt.Println(string(b))
+}
